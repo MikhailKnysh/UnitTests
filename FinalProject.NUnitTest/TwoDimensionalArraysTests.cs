41f9c7dd51c1d4d37f580ba74a3cdbfffb8df239
Mikhail <mishadnepr347@gmail.com>
2021-03-15 22:51:45 +0200

    Separated negative tests into different methods in TwoDimArrTests

--- FinalProject.NUnitTest/TwoDimensionalArraysTests.cs
+++ FinalProject.NUnitTest/TwoDimensionalArraysTests.cs
@@ -6,23 +6,6 @@ namespace FinalProject.NUnitTest
 {
     class TwoDimensionalArraysTests
     {
-        [TestCase(5)]
-        [TestCase(6)]
-        public void AllMethods_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
-            int mockNumber)
-        {
-            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
-
-            Assert.Throws<ArgumentException>(() =>
-            {
-                TwoDimensionalArrays.FindMinElementOfTheTwoDimArray(matrix);
-                TwoDimensionalArrays.FindMaxElementOfTheTwoDimArray(matrix);
-                TwoDimensionalArrays.FindIndexOfMinElementOfTheTwoDimArray(matrix);
-                TwoDimensionalArrays.FindIndexOfMaxElementOfTheTwoDimArray(matrix);
-                TwoDimensionalArrays.TransposeMatrix(matrix);
-            });
-        }
-
         [TestCase(1, -545)]
         [TestCase(2, -545)]
         [TestCase(3, -434)]
@@ -35,6 +18,17 @@ namespace FinalProject.NUnitTest
             int actual = TwoDimensionalArrays.FindMinElementOfTheTwoDimArray(mockArr);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5)]
+        [TestCase(6)]
+        public void FindMinElementOfTheTwoDimArray_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.FindMinElementOfTheTwoDimArray(matrix));
         }
 
         [TestCase(1, 67889)]
@@ -51,6 +45,17 @@ namespace FinalProject.NUnitTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(5)]
+        [TestCase(6)]
+        public void FindMaxElementOfTheTwoDimArray_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.FindMaxElementOfTheTwoDimArray(matrix));
+        }
+
         [TestCase(1, new int[] { 0, 4 })]
         [TestCase(2, new int[] { 0, 4 })]
         [TestCase(3, new int[] { 1, 0 })]
@@ -65,6 +70,17 @@ namespace FinalProject.NUnitTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(5)]
+        [TestCase(6)]
+        public void FindIndexOfMinElementOfTheTwoDimArray_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.FindIndexOfMinElementOfTheTwoDimArray(matrix));
+        }
+
         [TestCase(1, new int[] { 3, 3 })]
         [TestCase(2, new int[] { 0, 2 })]
         [TestCase(3, new int[] { 2, 0 })]
@@ -77,6 +93,17 @@ namespace FinalProject.NUnitTest
             int[] actual = TwoDimensionalArrays.FindIndexOfMaxElementOfTheTwoDimArray(mockArr);
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5)]
+        [TestCase(6)]
+        public void FindIndexOfMaxElementOfTheTwoDimArray_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.FindIndexOfMaxElementOfTheTwoDimArray(matrix));
         }
 
         [TestCase(1, 4)]
@@ -94,6 +121,16 @@ namespace FinalProject.NUnitTest
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(6)]
+        public void GetCounterOfAllSmallestNeighbours_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.GetCounterOfAllSmallestNeighbours(matrix));
+        }
+
         [TestCase(1, 7)]
         [TestCase(2, 8)]
         [TestCase(3, 9)]
@@ -106,6 +143,17 @@ namespace FinalProject.NUnitTest
             int[,] mockArrExpected = TwoDimArrayMock.GetMock(mockNumberExpected);
 
             Assert.AreEqual(mockArrExpected, mockArrActual);
+        }
+
+        [TestCase(5)]
+        [TestCase(6)]
+        public void AllMethods_WhenArrayIsEmptyOrNull_ShouldThrowArgumentException(
+            int mockNumber)
+        {
+            int[,] matrix = TwoDimArrayMock.GetMock(mockNumber);
+
+            Assert.Throws<ArgumentException>(() =>
+                TwoDimensionalArrays.TransposeMatrix(matrix));
         }
     }
 
